6ac7deef52c93a364445e42af1eb09843a18b369
Fadi AT <fadiatamny@gmail.com>
2019-12-18 00:09:04 +0200

    added chalk

--- Homework/hw3/app.js
+++ Homework/hw3/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require('dotenv').config();
 const morgan = require('morgan');
 const fs = require('fs');
+const chalk = require('chalk');
 const apiRoute = require('./routers/apiRouter');
 
 const app = express();
@@ -11,7 +12,18 @@ app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms', {
     stream: fs.createWriteStream('./access.log', {flags: 'a'})
 }));
-app.use(morgan('dev'));
+
+const morganMiddleware = morgan(function (tokens, req, res) {
+    return [
+        chalk.hex('#34ace0').bold(tokens.method(req, res)),
+        chalk.hex('#ffb142').bold(tokens.status(req, res)),
+        chalk.hex('#ff5252').bold(tokens.url(req, res)),
+        chalk.hex('#2ed573').bold(tokens['response-time'](req, res) + ' ms'),
+        '\n',
+    ].join(' ');
+});
+
+app.use(morganMiddleware);
 
 app.use('/api',apiRoute);
 
